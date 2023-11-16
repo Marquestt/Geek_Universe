36678a250b15e998ba7e7963e61163d6033eb1a2
ThallesPalmarim <125374007+ThallesPalmarim@users.noreply.github.com>
2023-11-15 21:20:00 -0300

    Update thalles.js

--- js/thalles.js
+++ js/thalles.js
@@ -3,7 +3,7 @@ sinopse.addEventListener("click", () =>{
         const texto = document.getElementById("texto");
         const titulo = document.getElementById("titulo");
         texto.innerText = "Grande clássico rpg do SNES, e se a linha Final Fantasy se tornou o que é hoje, essa grandiosidade teve sua explosão nesse jogo.";
-        titulo.innerText = "Sinópse";
+        titulo.innerText = "Sinopse";
     });
 
 const dados = document.getElementById("dados");
@@ -12,4 +12,12 @@ dados.addEventListener("click", () =>{
         const titulo = document.getElementById("titulo");
         texto.innerText = "Dados miscelaneos do jogo";
         titulo.innerText = "Dados";
+    });
+
+const imagens = document.getElementById("imagens");
+imagens.addEventListener("click", () =>{
+        const texto = document.getElementById("texto");
+        const titulo = document.getElementById("titulo");
+        texto.innerHTML = '<img src="imagens/ff6-1.png" class = "jogo" alt="foto de gameplay do jogo"><img src="imagens/ff6-2.png" class = "jogo" alt="foto de gameplay do jogo"><img src="imagens/ff6-3.png" class = "jogo" alt="foto de gameplay do jogo"><img src="imagens/ff6-4.png" class = "jogo" alt="foto de gameplay do jogo"><img src="imagens/ff6-5.png" class = "jogo" alt="foto de gameplay do jogo"><img src="imagens/ff6-6.png" class = "jogo" alt="foto de gameplay do jogo">';
+        titulo.innerText = "Imagens do Jogo";
     });

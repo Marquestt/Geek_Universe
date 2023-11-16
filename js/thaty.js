7b5dbad09495ab215b2ea7f6f03660b068ee7050
ThallesPalmarim <125374007+ThallesPalmarim@users.noreply.github.com>
2023-11-15 21:20:14 -0300

    Update thaty.js

--- js/thaty.js
+++ js/thaty.js
@@ -13,3 +13,11 @@ dados.addEventListener("click", () =>{
         texto.innerText = "Dados miscelaneos do jogo";
         titulo.innerText = "Dados";
     });
+
+const imagens = document.getElementById("imagens");
+imagens.addEventListener("click", () =>{
+        const texto = document.getElementById("texto");
+        const titulo = document.getElementById("titulo");
+        texto.innerHTML = '<img src="imagens/ts4-1.png" class = "jogo" alt="foto de gameplay do jogo"><img src="imagens/ts4-2.png" class = "jogo" alt="foto de gameplay do jogo"><img src="imagens/ts4-3.png" class = "jogo" alt="foto de gameplay do jogo"><img src="imagens/ts4-4.png" class = "jogo" alt="foto de gameplay do jogo"><img src="imagens/ts4-5.png" class = "jogo" alt="foto de gameplay do jogo"><img src="imagens/ts4-6.png" class = "jogo" alt="foto de gameplay do jogo">';
+        titulo.innerText = "Imagens do Jogo";
+    });

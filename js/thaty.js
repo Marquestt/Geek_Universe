66c17ef5693dbe16d9c03aa5e622ae1af1035607
ThallesPalmarim <125374007+ThallesPalmarim@users.noreply.github.com>
2023-11-21 13:59:37 -0300

    Update thaty.js

--- js/thaty.js
+++ js/thaty.js
@@ -3,7 +3,7 @@ sinopse.addEventListener("click", () =>{
         const texto = document.getElementById("texto");
         const titulo = document.getElementById("titulo");
         texto.innerText = " Um jogo onde voce cuida da sua vida, mas a de mentirinha, porque a de verdade voce está deixando pra lá pra jogar isso aqui";
-        titulo.innerText = "Sinópse";
+        titulo.innerHTML = '<h3>Jogabilidade</h3>';
     });
 
 const dados = document.getElementById("dados");

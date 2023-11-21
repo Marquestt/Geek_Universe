efc2c696ee8502a3532534bf0fdd11289ba55710
ThallesPalmarim <125374007+ThallesPalmarim@users.noreply.github.com>
2023-11-21 14:05:40 -0300

    Update joao.js

--- js/joao.js
+++ js/joao.js
@@ -19,7 +19,7 @@ imagens.addEventListener("click", () =>{
         const texto = document.getElementById("texto");
         const titulo = document.getElementById("titulo");
         texto.innerHTML = '<img src="imagens/terraria-1.png" class = "jogo" alt="foto de gameplay do jogo"><img src="imagens/terraria-2.png" class = "jogo" alt="foto de gameplay do jogo"><img src="imagens/terraria-3.png" class = "jogo" alt="foto de gameplay do jogo"><img src="imagens/terraria-4.png" class = "jogo" alt="foto de gameplay do jogo"><img src="imagens/terraria-5.png" class = "jogo" alt="foto de gameplay do jogo"><img src="imagens/terraria-6.png" class = "jogo" alt="foto de gameplay do jogo">';
-        titulo.innerText = "Imagens do Jogo";
+        titulo.innerHTML = '<h3>Imagens do Jogo</h3>';
     });
 
 

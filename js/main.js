33e3c3770772102dccc1c6aa5c75c919c7e98f3e
ThallesPalmarim <125374007+ThallesPalmarim@users.noreply.github.com>
2023-11-21 11:30:50 -0300

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const fieldset = document.getElementById("fieldset");
+const botao = document.getElementById("add");
+const entrada = document.getElementById("entrada");
+const lista = document.createElement("ul");
+
+fieldset.appendChild(lista);
+
+botao.addEventListener("click", function() {
+    const jogo = entrada.value;
+    const item = document.createElement("li");
+	const deletar = document.createElement("button");
+    //deletar.textContent = "Delete";
+	//deletar.style.display = 'none';
+    item.textContent = jogo;
+	//item.appendChild(deletar);
+    lista.appendChild(item);
+    entrada.value = '';
+	//deletar.addEventListener("click", function(event) {
+        //event.stopPropagation();
+        //lista.removeChild(item);
+    //});
+	//item.addEventListener("click", function() {
+        //deletar.style.display = deletar.style.display === 'none' ? '' : 'none';
+    //});
+});
